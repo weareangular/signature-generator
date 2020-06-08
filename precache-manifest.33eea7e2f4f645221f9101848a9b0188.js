116669e54f0cc7509d836c5cccb8055141a55354
iamjuandis <judavidpe@gmail.com>
2020-06-08 14:18:59 +0000

    Deploying to gh-pages from  @ 59282565fee47e683133a81d065f08bd213ece3a 🚀

---
+++ precache-manifest.33eea7e2f4f645221f9101848a9b0188.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe76c52d36990a7be74ced916e624037",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05ef64c564ab8d7f207b",
+    "url": "/static/css/main.db221a30.chunk.css"
+  },
+  {
+    "revision": "1d02c7eea96c6db01d1c",
+    "url": "/static/js/2.3de2043a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3de2043a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ef64c564ab8d7f207b",
+    "url": "/static/js/main.c79d6c2c.chunk.js"
+  },
+  {
+    "revision": "fead98a42cb2a9e3d376",
+    "url": "/static/js/runtime-main.65d21f7c.js"
+  },
+  {
+    "revision": "318e2b86fd68b871a8a1d6e2b2ff353d",
+    "url": "/static/media/angular-logo-new-black.318e2b86.svg"
+  }
+]);
